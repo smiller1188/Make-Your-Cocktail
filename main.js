ad813ed270168885950fc750bf0405f3a79dd247
Sarah Miller <97858648+smiller1188@users.noreply.github.com>
2022-09-25 20:30:35 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink() {
+  let drink = document.querySelector('input').value
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.drinks)
+    document.querySelector('h2').innerText = data.drinks[0].strDrink
+    document.querySelector('img').src = data.drinks[0].strDrinkThumb
+    document.querySelector('h3').innerText = data.drinks[0].strInstructions
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  })
+}
